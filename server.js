941f78084453f32c6e636f09f2144db155d7242a
Philippe Luchansky <philuchansky@gmail.com>
2018-06-29 00:21:35 -0700

    server static html with milligram.min.css and main.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const
   app = express()
   PORT = process.env.PORT || 3000
 
+app.use(express.static('public'))
+
 app.listen(PORT, (err) => {
   console.log(err || `Server running on ${PORT}.`)
 })
